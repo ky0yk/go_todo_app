80e5d171b766672d3603b00fe2a0706cd57fcfa5
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-05-27 10:03:52 +0900

    クエリの間違いを修正

--- handler/service.go
+++ handler/service.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ky0yk/go_todo_app/entity"
 )
 
-//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService
+//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService RegisterUserService
 type ListTasksService interface {
 	ListTasks(ctx context.Context) (entity.Tasks, error)
 }
--- store/user.go
+++ store/user.go
@@ -13,8 +13,8 @@ func (r *Repository) RegisterUser(ctx context.Context, db Execer, u *entity.User
 	u.Created = r.Clocker.Now()
 	u.Modified = r.Clocker.Now()
 	sql := `INSERT INTO user (
-		name, password, role, created, modified
-	) VALUES (?, ?, ?, ?)`
+name, password, role, created, modified
+) VALUES (?, ?, ?, ?, ?)`
 	result, err := db.ExecContext(ctx, sql, u.Name, u.Password, u.Role, u.Created, u.Modified)
 	if err != nil {
 		var mysqlErr *mysql.MySQLError
